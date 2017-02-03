9ce51c940cd57af769fa7bc44db2d21776d4d860
Brianne Barta <brianne28@ufl.edu>
2017-02-03 17:51:28 -0500

    add function expression

--- js/main.js
+++ js/main.js
@@ -26,3 +26,18 @@ function howdy() {
 
 howdy();
 console.log(howdy());
+
+var numba = function (string) {
+
+    if (string.length < 7) {
+        console.log('What a short little word!');
+    } else if (string.length > 7) {
+        console.log("I'm sorry but that's too many to count!");
+    } else {
+        console.log('7, what a perfect choice');
+    }
+};
+
+console.log(numba('7'));
+console.log(numba('seventy'));
+console.log(numba('university'));
