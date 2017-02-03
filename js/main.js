eb3365f3502c6afa7f6cf9d6814d5ec2a373a96c
Brianne Barta <brianne28@ufl.edu>
2017-02-03 07:29:04 -0500

    add presidents branch and content

--- js/main.js
+++ js/main.js
@@ -3,3 +3,18 @@ var iceCream = [
     'Cookie Dough', ' Neapolitan', ' Chocolate Chip',
     ' Vanilla Bean'
 ];
+
+// presidents
+
+var presidents = [
+
+        {firstName: 'Donald', lastName: 'Trump', termLength: 'TBD', party: 'Republican', yearsOfPresidency: '2017-'},
+        {firstName: 'Barack', lastName: 'Obama', termLength: '2', party: 'Democratic', yearsOfPresidency: '2009-2017'},
+        {firstName: 'George', lastName: 'W. Bush', termLength: '2', party: 'Republican', yearsOfPresidency: '2001-2009'},
+        {firstName: 'Bill', lastName: 'Clinton', termLength: '2', party: 'Democratic', yearsOfPresidency: '1993-2001'},
+        {firstName: 'George', lastName: 'H.W. Bush', termLength: '1', party: 'Republican', yearsOfPresidency: '1989-1993'}
+
+];
+
+
+console.log(presidents[2].firstName + ' ' + presidents[2].lastName);
